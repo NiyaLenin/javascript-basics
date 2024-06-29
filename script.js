c72dfe524042bd3d0bc0bacca86d3768ec371850
NiyaLenin <niyaleninkr@gmail.com>
2024-06-29 10:51:53 +0530

    datatype Config

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+console.log("hello world");
+var a = 10;
+console.log(a);
+var a ="name"
+console.log(a);
+const pi = 3.14;
+console.log(pi);
+var numbers = [1, 2, 3, 4, 5];
+var newNumber = [...numbers,6,7,8];
+console.log(newNumber);
+let valueone=2;
+let valuetwo= "a";
+console.log(valueone+valuetwo);
+console.log(valueone+parseInt(valuetwo));
+const name = "john";
+const age=30;
+const rating = 4.5;
+const iscool=true;
+const k=null;
+var y;
+console.log(typeof name);
+console.log("my name is" + name +"and i am "+age);
